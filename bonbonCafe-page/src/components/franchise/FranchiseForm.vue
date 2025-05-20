7700633d4a118e8998e1fe85e07fd4aaf250a344
jelee55 <jegyeong25@gmail.com>
2025-05-20 10:09:50 +0900

    [feat] franchise 등록 화면 구현중 (이제경)

--- bonbonCafe-page/src/components/franchise/FranchiseForm.vue
+++ bonbonCafe-page/src/components/franchise/FranchiseForm.vue
@@ -1,58 +1,64 @@
-<template>
+ <template>
     <form @submit.prevent="handleSubmit">
         <v-table class="pa-0 d-flex">
         <tbody>
           <tr>
-            <td class="test-left thead-title tb-bg"><span class="required">* </span> 가맹점 이름</td>
-            <td class="test-left"><input type="text"/></td>
-            <td class="test-left thead-title"><span class="required">* </span> 점주 이름</td>
-            <td class="test-left"><input type="text"/></td>
+            <td class=" thead-title tb-bg"><span class="required">* </span> 가맹점 이름</td>
+            <td class=""><input type="text"/></td>
+            <td class=" thead-title"><span class="required">* </span> 점주 이름</td>
+            <td class=""><input type="text"/></td>
           </tr>
           <tr>
-            <td class="test-left thead-title "><span class="required">* </span> 가맹점 사진</td>
+            <td class=" thead-title "><span class="required">* </span> 가맹점 사진</td>
               <td colspan="1">
                 <div class="ma-5 upload-box">사진 올리기</div>
                 <input type="file" class="file-input" />
               </td>
-            <td class="test-left thead-title"><span class="required">* </span> 점주 사진</td>
+            <td class=" thead-title"><span class="required">* </span> 점주 사진</td>
             <td colspan="1">
                 <div class="upload-box">사진 올리기</div>
                 <input type="file" class="file-input" />
               </td>
           </tr>
           <tr>
-            <td class="test-left thead-title"><span class="required">* </span> 가맹점 전화번호</td>
-            <td class="test-left"><input type="tel"/></td>
-            <td class="test-left thead-title"><span class="required">* </span> 담당자</td>
-            <td class="test-left"><input type="text"/></td>
+            <td class=" thead-title"><span class="required">* </span> 가맹점 전화번호</td>
+            <td class=""><input type="tel"/></td>
+            <td class=" thead-title"><span class="required">* </span> 담당자</td>
+            <td>
+              <select>
+                <option disabled selected>선택</option>
+                <option>홍길동</option>
+              </select>
+            </td>
           </tr>
           <tr>
-            <td class="test-left thead-title"><span class="required">* </span> 주소</td>
-            <td class="test-left" rowspan="2">
-                <v-text-field
-                  append-inner-icon="mdi-map-marker"
-                  label="도로명 입력"
-                ></v-text-field>
-              </td>
-            <td class="test-left thead-title"> 주차 가능 여부</td>
-            <td class="test-left">ddd</td>
+            <td class="thead-title"><span class="required">* </span>우편 번호</td>
+            <td>
+              <input type="text" />
+              <button class="search-btn">🔍</button>
+            </td>
+            <td class="thead-title">주차 가능</td>
+            <td>
+              <label><input type="checkbox" /> 가능</label>
+              <label><input type="checkbox" /> 불가</label>
+            </td>
           </tr>
           <tr>
-            <td class="test-left thead-title"></td>
-            <td class="test-left thead-title"><span class="required">* </span> 운영상태</td>
-            <td class="test-left">ddd</td>
+            <td class=" thead-title"></td>
+            <td class=" thead-title"><span class="required">* </span> 운영상태</td>
+            <td class="">ddd</td>
           </tr>
           <tr>
-            <td class="test-left thead-title"></td>
-            <td class="test-left">ddd</td>
-            <td class="test-left thead-title"><span class="required">* </span> 운영 시간</td>
-            <td class="test-left">ddd</td>
+            <td class=" thead-title"></td>
+            <td class="">ddd</td>
+            <td class=" thead-title"><span class="required">* </span> 운영 시간</td>
+            <td class="">ddd</td>
           </tr>
           <tr>
-            <td class="test-left thead-title"><span class="required">* </span> 개업 일자 </td>
-            <td class="test-left">ddd</td>
-            <td class="test-left thead-title">* 좌석수</td>
-            <td class="test-left">ddd</td>
+            <td class=" thead-title"><span class="required">* </span> 개업 일자 </td>
+            <td class="">ddd</td>
+            <td class=" thead-title"> 좌석수</td>
+            <td class="">ddd</td>
           </tr>
         </tbody>
         </v-table>
