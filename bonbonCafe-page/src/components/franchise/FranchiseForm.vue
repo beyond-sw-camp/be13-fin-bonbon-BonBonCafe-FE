c291b2d0148d8ccf2665a1557df24de80f9656c9
jelee55 <jegyeong25@gmail.com>
2025-05-22 16:02:22 +0900

    test

--- bonbonCafe-page/src/components/franchise/FranchiseForm.vue
+++ bonbonCafe-page/src/components/franchise/FranchiseForm.vue
@@ -1,165 +1,195 @@
 <template>
-  <v-container class="container">
-    <div class="franchiseTable">
-      <div class="cell headerC" style="height: 70px;"><strong><span class="requierd">*</span>가맹점 이름</strong></div>
-      <div class="cell"  style="height: 70px;">
-        <v-text-field 
-            density="comfortable"
-            variant="outlined"
-            class="textBox"
-          />
-      </div>
-      <div class="cell headerC" style="height: 70px;"><strong><span class="requierd">*</span>점주 이름</strong></div>
-      <div class="cell" style="height: 70px;">
-        <v-text-field 
-            density="comfortable"
-            variant="outlined"
-            class="textBox"
-          />
-      </div>
+  <form @submit.prevent="SubmitEvent">
+    <v-container class="container">
+      <div class="franchiseTable">
+        <div class="cell headerC" style="height: 70px;"><strong><span class="requierd">*</span>가맹점 이름</strong></div>
+        <div class="cell"  style="height: 70px;">
+          <v-text-field 
+              v-model="form.franchiseName"
+              density="comfortable"
+              variant="outlined"
+              class="textBox"
+            />
+        </div>
+        <div class="cell headerC" style="height: 70px;"><strong><span class="requierd">*</span>점주 이름</strong></div>
+        <div class="cell" style="height: 70px;">
+          <v-text-field 
+              v-model="form.franchiseeName"
+              density="comfortable"
+              variant="outlined"
+              class="textBox"
+            />
+        </div>
 
-      <div class="cell headerC" style="height: 300px;"><strong><span class="requierd">*</span>가맹점 사진</strong></div>
-      <div class="cell">
-        <v-file-input
-        class="image"
-        accept="image/*"
-        label="이미지 업로드"
-        @change="onFranchiseImageChange"
-      />
-      <img
-        v-if="franchiseImageUrl"
-        :src="franchiseImageUrl"
-        alt="가맹점 이미지 미리보기"
-        class="preview-img"
-      />
-      </div>
-      <div class="cell headerC"><strong><span class="requierd">*</span>점주 사진</strong></div>
-      <div class="cell">
-        <v-file-input
-        class="image"
-        accept="image/*"
-        label="이미지 업로드"
-        @change="onOwnerImageChange"
-      />
-      <img
-        v-if="ownerImageUrl"
-        :src="ownerImageUrl"
-        alt="점주 이미지 미리보기"
-        class="preview-img"
-      />
-      </div>
+        <div class="cell headerC" style="height: 300px;"><strong><span class="requierd">*</span>가맹점 사진</strong></div>
+        <div class="cell">
+          <v-file-input
+          class="image"
+          accept="image/*"
+          label="이미지 업로드"
+          @change="onFranchiseImageChange"
+        />
+        <img
+          v-if="franchiseImageUrl"
+          :src="franchiseImageUrl"
+          alt="가맹점 이미지 미리보기"
+          class="preview-img"
+        />
+        </div>
+        <div class="cell headerC"><strong><span class="requierd">*</span>점주 사진</strong></div>
+        <div class="cell">
+          <v-file-input
+          class="image"
+          accept="image/*"
+          label="이미지 업로드"
+          @change="onOwnerImageChange"
+        />
+        <img
+          v-if="ownerImageUrl"
+          :src="ownerImageUrl"
+          alt="점주 이미지 미리보기"
+          class="preview-img"
+        />
+        </div>
 
-      <div class="cell headerC"  style="height: 70px;"><strong><span class="requierd">*</span>가맹점 연락처</strong></div>
-      <div class="cell" style="height: 70px;">
-        <v-text-field type="tel" density="comfortable" variant="outlined" class=" textBox" />
-      </div>
-      <div class="cell headerC" style="height: 70px;"><strong><span class="requierd">*</span>담당자</strong></div>
-      <div class="cell" style="height: 70px;">
-        <v-select
-            :items="['홍길동']"
-            placeholder="선택"
-            density="comfortable"
-            variant="outlined"
-            class=" textBox"
-          ></v-select>
-      </div>
+        <div class="cell headerC"  style="height: 70px;"><strong><span class="requierd">*</span>가맹점 연락처</strong></div>
+        <div class="cell" style="height: 70px;">
+          <v-text-field v-model="franchiseTel" type="tel" density="comfortable" variant="outlined" class=" textBox" />
+        </div>
+        <div class="cell headerC" style="height: 70px;"><strong><span class="requierd">*</span>담당자</strong></div>
+        <div class="cell" style="height: 70px;">
+          <v-select
+              :items="['홍길동']"
+              placeholder="선택"
+              density="comfortable"
+              variant="outlined"
+              class=" textBox"
+            ></v-select>
+        </div>
 
-      <div class="cell headerC" style="height: 210px;"><strong><span class="requierd">*</span>주소</strong></div>
-      <div class="cell"  style="height: 210px;">
-        <KakaoAPI />
-      </div>
-      <div class="cell3 " style="height: 210px;">
-        <div class="ma-0 pa-0">
-          <div style="height: 70px; border: 1px solid #ccc; background-color: #efefef; color: gray; padding: 8px;">
-            <strong><span class="requierd">*</span>개업 일자</strong>
-          </div>
-          <div style="height: 70px; border: 1px solid #ccc; background-color: #efefef; color: gray; padding: 8px;">
-            <strong><span class="requierd">*</span>운영 상태</strong>
-          </div>
-          <div style="height: 70px; border: 1px solid #ccc; background-color: #efefef; color: gray; padding: 8px;">
-            <strong><span class="requierd">*</span>운영 시간</strong>
+        <div class="cell headerC" style="height: 210px;"><strong><span class="requierd">*</span>주소</strong></div>
+        <div class="cell"  style="height: 210px;">
+          <KakaoAPI />
+        </div>
+        <div class="cell3 " style="height: 210px;">
+          <div class="ma-0 pa-0">
+            <div style="height: 70px; border: 1px solid #ccc; background-color: #efefef; color: gray; padding: 8px;">
+              <strong><span class="requierd">*</span>개업 일자</strong>
+            </div>
+            <div style="height: 70px; border: 1px solid #ccc; background-color: #efefef; color: gray; padding: 8px;">
+              <strong><span class="requierd">*</span>운영 상태</strong>
+            </div>
+            <div style="height: 70px; border: 1px solid #ccc; background-color: #efefef; color: gray; padding: 8px;">
+              <strong><span class="requierd">*</span>운영 시간</strong>
+            </div>
           </div>
         </div>
-      </div>
-      <div class="cel3">
-        <div>
-          <div class="d-flex align-center" style="height: 70px; border: 1px solid #ccc; padding: 8px; padding-top: 30px;">
-            <v-select
-                    :items="['2023', '2024', '2025']" 
-                    label="년" 
+        <div class="cel3">
+          <div>
+            <div class="d-flex align-center" style="height: 70px; border: 1px solid #ccc; padding: 8px; padding-top: 30px;">
+              <v-select
+                :items="['2023', '2024', '2025']" 
+                label="년" 
+                density="comfortable"
+                variant="outlined"
+                class=" textBox mr-2"
+
+              />
+              <v-select 
+                :items="['01', '02', '03']" 
+                label="월" 
+                density="comfortable"
+                variant="outlined"
+                class=" textBox mr-2"
+
+            />
+              <v-select 
+                :items="['01', '02', '03']" 
+                label="일" 
+                density="comfortable"
+                variant="outlined"
+                class=" textBox "
+              />
+
+            </div>
+            <div class="d-flex align-center"  style="height: 70px; border: 1px solid #ccc; padding: 8px; padding-top: 30px">
+              <v-checkbox label="운영 중" value="운영중" dense class="checkbox"></v-checkbox>
+              <v-checkbox label="운영 중단" value="중단" dense class="checkbox"></v-checkbox>
+            </div>
+            <div class="d-flex align-center" style="height: 70px; border: 1px solid #ccc; padding: 8px; padding-top: 30px">
+                  <v-text-field 
+                    type="time" 
                     density="comfortable"
                     variant="outlined"
-                    class=" textBox mr-2"
-
+                    class="mr-2 textBox"
                   />
-                  <v-select 
-                    :items="['01', '02', '03']" 
-                    label="월" 
+                  <span class="mx-2">~</span>
+                  <v-text-field
+                    type="time" 
                     density="comfortable"
                     variant="outlined"
-                    class=" textBox mr-2"
-
-                />
-                  <v-select 
-                    :items="['01', '02', '03']" 
-                    label="일" 
-                    density="comfortable"
-                    variant="outlined"
-                    class=" textBox "
+                    class=" textBox"
                   />
-
-          </div>
-          <div class="d-flex align-center"  style="height: 70px; border: 1px solid #ccc; padding: 8px; padding-top: 30px">
-            <v-checkbox label="운영 중" value="운영중" dense class="checkbox"></v-checkbox>
-            <v-checkbox label="운영 중단" value="중단" dense class="checkbox"></v-checkbox>
-          </div>
-          <div class="d-flex align-center" style="height: 70px; border: 1px solid #ccc; padding: 8px; padding-top: 30px">
-                <v-text-field 
-                  type="time" 
-                  density="comfortable"
-                  variant="outlined"
-                  class="mr-2 textBox"
-                />
-                <span class="mx-2">~</span>
-                <v-text-field
-                  type="time" 
-                  density="comfortable"
-                  variant="outlined"
-                  class=" textBox"
-                />
+            </div>
           </div>
         </div>
+
+        <div class="cell headerC" style="height: 70px;"><strong><span class="requierd">*</span>주차 여부</strong></div>
+        <div class="cell d-flex align-center" style="height: 70px;">
+          <v-checkbox label="가능" value="가능" class="checkbox"/>
+          <v-checkbox label="불가" value="불가" class="checkbox"/>
+
+        </div>
+        <div class="cell headerC" style="height: 70px;"><strong><span class="requierd">*</span>좌석수</strong></div>
+        <div class="cell" style="height: 70px;">
+          <v-text-field 
+              type="number"
+              density="comfortable"
+              variant="outlined"
+              class="textBox"
+              dense
+            />      
+        </div>
       </div>
-
-      <div class="cell headerC" style="height: 70px;"><strong><span class="requierd">*</span>주차 여부</strong></div>
-      <div class="cell d-flex align-center" style="height: 70px;">
-        <v-checkbox label="가능" value="가능" class="checkbox"/>
-        <v-checkbox label="불가" value="불가" class="checkbox"/>
-
-      </div>
-      <div class="cell headerC" style="height: 70px;"><strong><span class="requierd">*</span>좌석수</strong></div>
-      <div class="cell" style="height: 70px;">
-        <v-text-field 
-            type="number"
-            density="comfortable"
-            variant="outlined"
-            class="textBox"
-            dense
-          />      
-
-      </div>
-  </div>
-
-  </v-container>
-  </template>
+    </v-container>
+    <v-btn type="submit">제출</v-btn>
+  </form>
+</template>
 
 <script setup>
 import KakaoAPI from './KakaoAPI.vue';
+import { ref,reactive } from 'vue'
 
-import { ref } from 'vue'
+// const managerName = ref({});
 
-const franchiseImageUrl = ref(null)
-const ownerImageUrl = ref(null)
+const form = reactive({
+  franchiseName: '',
+  franchiseeName: '',
+  franchiseTel: '',
+  managerName: '',
+  openYear: '',
+  openMonth: '',
+  openDay: '',
+  status: '', // 운영중 or 중단
+  openTime: '',
+  closeTime: '',
+  parkingAvailability: '', // 가능/불가
+  seatingCapacity: 0,
+  franchiseImage: null,
+  franchiseeImage: null,
+  address: '', // KakaoAPI로 전달받은 주소
+});
+
+
+
+const emit = defineEmits(['submit']);
+
+function SubmitEvent() {
+  emit('submit', form)
+};
+
+const franchiseImageUrl = ref(null);
+const ownerImageUrl = ref(null);
 
 const onFranchiseImageChange = (fileList) => {
   const file = fileList instanceof FileList ? fileList[0] : fileList
@@ -174,6 +204,8 @@ const onOwnerImageChange = (fileList) => {
     ownerImageUrl.value = URL.createObjectURL(file)
   }
 }
+
+
 </script>
 
 <style scoped>
--- bonbonCafe-page/src/views/franchise/FranchiseRegister.vue
+++ bonbonCafe-page/src/views/franchise/FranchiseRegister.vue
@@ -4,7 +4,7 @@
         <h2>가맹점 등록</h2>
       </v-col>
       <v-col>
-        <FranchiseForm />
+        <FranchiseForm ref="franchiseFormRef" @submit="onSubmitFranchise" />
       </v-col>
       <v-col class="btnBox">
         <v-btn  
@@ -34,6 +34,27 @@
 
 <script setup>
 import FranchiseForm from '@/components/franchise/FranchiseForm.vue';
+
+import { ref } from 'vue'
+import apiClient from '@/api' // axios 인스턴스
+
+const franchiseFormRef = ref(null)
+
+const onSubmitFranchise = async (formData) => {
+  try {
+    console.log(formData);
+    
+    const response = await apiClient.post('/franchise', formData)
+    console.log('등록 성공:', response.data)
+    // 성공 후 처리 (예: 페이지 이동)
+  } catch (error) {
+    console.error('등록 실패:', error)
+  }
+}
+
+const submitForm = () => {
+  franchiseFormRef.value?.handleSubmit()
+}
 
 </script>
 
